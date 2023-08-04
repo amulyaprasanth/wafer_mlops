7f611759e52ddd9c11a29bad26b535e63b3a34b2
Nadagani Amulya Prasanth <75437956+amulyaprasanth@users.noreply.github.com>
2023-08-04 23:26:35 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is implementation of opensource mlops',
+    author='amulyaprasanth',
+    license='MIT',
+)
